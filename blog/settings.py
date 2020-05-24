f82d839df74cc11026beaf8d1425907edbf5d26a
marshallclaudius <marshallclaudius@gmail.com>
2020-05-25 00:24:45 +0530

    Third commit

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER':'postgres',
+        'PASSWORD':'root',
+        'HOST':'localhost'
     }
 }
 
@@ -123,3 +127,6 @@ STATICFILES_DIRS=[
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- travello/views.py
+++ travello/views.py
@@ -5,27 +5,6 @@ from .models import Destination
 # Create your views here.
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = "The City never sleeps"
-    dest1.img = 'destination_1.jpg'
-    dest1.offer = False
-    dest1.price = 2500
-
-    dest2 = Destination()
-    dest2.name = 'Ulhasnagar'
-    dest2.desc = 'Thane ka nallah'
-    dest2.img = 'destination_2.jpg'
-    dest2.offer = True
-    dest2.price = 1564
-
-    dest3 = Destination()
-    dest3.name = 'Worli Gaon'
-    dest3.desc = 'Kinny ka ghar'
-    dest3.img = 'destination_3.jpg'
-    dest3.offer = False
-    dest3.price = 3001
-
-    dests = [dest1,dest2,dest3]
+    dests = Destination.objects.all()
     return render(request, 'index.html',{'dests' : dests})
 
